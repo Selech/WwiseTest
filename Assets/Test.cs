3ef3a83f67df9e6836a4e8c0938e2f80c68104f9
Peter Scheel <selech07@gmail.com>
2016-09-23 12:39:07 +0200

    Wwise plugin

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TestEvent(){
+		AkSoundEngine.PostEvent("Play_Bell",this.gameObject);
+	}
+}
