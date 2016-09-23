f372a08088e046a4f8e385b5220949019a5ee616
Peter Scheel <selech07@gmail.com>
2016-09-23 12:53:36 +0200

    Cleanup

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -16,4 +16,12 @@ public class Test : MonoBehaviour {
 	public void TestEvent(){
 		AkSoundEngine.PostEvent("Play_Bell",this.gameObject);
 	}
+
+	public void TestEvent2(){
+		AkSoundEngine.PostEvent("Play_Snap",this.gameObject);
+	}
+
+	public void TestEvent3(){
+		AkSoundEngine.PostEvent("Play_Ambience1",this.gameObject);
+	}
 }
